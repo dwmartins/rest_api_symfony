c1aad0011fd81d0bb0f4a24e81b161a8a6001214
Douglas Martins <douglas.martins@arcasolutions.com>
2023-11-17 11:17:21 -0300

    creating books

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,18 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BookController extends AbstractController
 {
-    #[Route('/books', name: 'app_book')]
-    public function index(): JsonResponse
+    #[Route('/books', name: 'books_list', methods: ['GET'])]
+    public function index(BookRepository $bookRepository): JsonResponse
     {
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/BookController.php',
+            'data' => $bookRepository->findAll(),
         ]);
+    }
+
+    #[Route('/books', name: 'books_create', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $em): JsonResponse
+    {
+        $data = $request->request->all();
+
+        $book = new Book();
+        $book->setTitle($data['title']);
+        $book->setIsbn($data['isbn']);
+        $book->setCreatedAt(new \DateTimeImmutable('now', new \DateTimeZone('America/Sao_Paulo')));
+        $book->setUpdatedAt(new \DateTimeImmutable('now', new \DateTimeZone('America/Sao_Paulo')));
+
+        $em->persist($book);
+        $em->flush();
+
+        return $this->json([
+            'message' => 'Book created successfully'
+        ], 201);
     }
 }
